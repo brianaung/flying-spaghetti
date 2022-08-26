bd8923f05ae64fbd57b480388d5a0b8305f9c948
zongliang han <zongliangh@student.unimelb.edu.au>
2022-08-26 17:30:26 +1000

    add route

--- app.js
+++ app.js
@@ -23,6 +23,38 @@ const app = express();
 // const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
+app.get('/', (req, res) => { 
+    res.send('Our demo app is working!') 
+});
+
+app.get('/about', (req, res) => { 
+    res.send('this is about page') 
+});
+
+app.get('/user:id', (req, res) => { 
+    res.send("<p>this is user" + req.params.id + "page!"+"</p>")
+     
+});
+
+app.get('/login', (req, res) => { 
+    res.send('this is login page') 
+});
+
+app.get('/register', (req, res) => { 
+    res.send('this is register page!') 
+});
+
+app.get('/upload', (req, res) => { 
+    res.send('this is upload page!') 
+});
+
+app.get('/photo', (req, res) => { 
+    res.send('this is photo page') 
+});
+
+
+
+
 app.listen(3000, () => { 
     console.log('Demo app is listening on port 3000!') 
 });
