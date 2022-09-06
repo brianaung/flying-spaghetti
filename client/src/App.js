ad81cc740bbf95c1b9cb8fad24067a2c905989ee
Brian Aung <soekhantaung16@gmail.com>
2022-09-06 17:47:31 +1000

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+
+// import components here
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
