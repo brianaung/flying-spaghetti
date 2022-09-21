c2ede23f91ad03e47feceae893f6634b83c52c21
Brian Aung <soekhantaung16@gmail.com>
2022-09-22 02:15:29 +1000

    make searchbar a seperate component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,18 @@
 import React from 'react';
+// mui components
+import { styled } from '@mui/system';
 import Button from '@mui/material/Button';
 import AppBar from '@mui/material/AppBar';
-import InputBase from '@mui/material/InputBase';
 import Toolbar from '@mui/material/Toolbar';
 import Avatar from '@mui/material/Avatar';
-import { styled } from '@mui/system';
+// my components
+import Searchbar from './Searchbar';
 
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
   justifyContent: 'space-between',
   gap: '50px'
 });
-
-const Search = styled('div')(({ theme }) => ({
-  backgroundColor: 'white',
-  borderRadius: theme.shape.borderRadius,
-  width: '150px',
-  marginRight: 'auto',
-  padding: '0 10px'
-}));
 
 const MenuBtn = styled(Button)({
   color: 'white'
@@ -41,9 +35,7 @@ export default function Navbar() {
           Username
         </User>
 
-        <Search>
-          <InputBase placeholder="SEARCH" />
-        </Search>
+        <Searchbar />
 
         <MenuBtn>Menu</MenuBtn>
       </StyledToolbar>
