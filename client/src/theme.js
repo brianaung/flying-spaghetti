af57ce9ce947170409c66c34aa37e46b8030d907
Brian Aung <soekhantaung16@gmail.com>
2022-09-22 02:16:06 +1000

    customize mui fonts

--- client/src/theme.js
+++ client/src/theme.js
@@ -8,5 +8,28 @@ export const theme = createTheme({
     secondary: {
       main: '#E8DB7D'
     }
+  },
+  typography: {
+    fontFamily: ['"Montserrat"', 'Roboto', 'Arial'].join(','),
+    h1: {
+      fontSize: '40px',
+      fontWeight: '700'
+    },
+    h2: {
+      fontSize: '32px',
+      fontWeight: '600'
+    },
+    h3: {
+      fontSize: '24px'
+    },
+    body1: {
+      fontSize: '16px'
+    },
+    body2: {
+      fontSize: '14px'
+    },
+    button: {
+      fontWeight: '600'
+    }
   }
 });
