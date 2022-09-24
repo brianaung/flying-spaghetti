04b12dc25b5ce41d79afb14eda876cca8a6cb7b6
Brian Aung <soekhantaung16@gmail.com>
2022-09-24 20:05:26 +1000

    remove a div around upload btn to remove extra whitespace + some other clean ups

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -9,7 +9,8 @@ import AddIcon from '@mui/icons-material/Add';
 import FolderFrame from '../components/FolderFrame';
 import PhotoFrame from '../components/PhotoFrame';
 import { Photo101 } from '../data/photo-data';
-import { Box, Modal, TextField, Button } from '@mui/material';
+import { Box, Modal, TextField } from '@mui/material';
+import SubmitBtn from './SubmitBtn';
 
 const FeedContainer = styled(Stack)(({ theme }) => ({
   gap: '50px',
@@ -21,22 +22,24 @@ const FeedContainer = styled(Stack)(({ theme }) => ({
   }
 }));
 
-const SubmitForm = styled(Box)({
+const SubmitForm = styled('form')({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '20px',
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: '30%',
   backgroundColor: 'white',
-  border: '1px solid #000',
-  boxShadow: 24,
+  border: '2px solid black',
   p: 4,
   padding: '20px'
 });
 
 // TODO: add current directory
 export default function Feed(props) {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [selectedImage, setSelectedImage] = useState(null);
@@ -60,38 +63,34 @@ export default function Feed(props) {
         <AddIcon />
       </Fab>
 
-      <div>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description">
-          <SubmitForm>
-            <form style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-              <input
-                accept="image/*"
-                type="file"
-                onChange={(e) => setSelectedImage(e.target.files[0])}
-              />
-              {imageUrl && selectedImage && (
-                <Box mt={2} mb={2} textAlign="center">
-                  <img src={imageUrl} alt={selectedImage.name} height="200px" />
-                </Box>
-              )}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <SubmitForm id="upload-form">
+          <input
+            accept="image/*"
+            type="file"
+            onChange={(e) => setSelectedImage(e.target.files[0])}
+          />
+          {imageUrl && selectedImage && (
+            <Box mt={2} mb={2} textAlign="center">
+              <img src={imageUrl} alt={selectedImage.name} height="200px" />
+            </Box>
+          )}
 
-              <TextField name="name" variant="outlined" label="Name" fullWidth></TextField>
-              <TextField
-                name="description"
-                variant="outlined"
-                label="Description"
-                fullWidth></TextField>
-              <Button variant="contained" color="primary" type="submit">
-                Upload
-              </Button>
-            </form>
-          </SubmitForm>
-        </Modal>
-      </div>
+          <TextField name="name" variant="outlined" label="Name" fullWidth></TextField>
+          <TextField
+            name="description"
+            variant="outlined"
+            label="Description"
+            fullWidth></TextField>
+
+          <SubmitBtn content="Upload" type="submit" />
+        </SubmitForm>
+      </Modal>
+
       <FolderFrame></FolderFrame>
       <PhotoFrame photos={Photo101} query={props.query}></PhotoFrame>
     </FeedContainer>
