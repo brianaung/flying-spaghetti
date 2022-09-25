7ae433b272afcc6b2f816a8525bc4a297cc71177
Brian Aung <soekhantaung16@gmail.com>
2022-09-25 14:18:52 +1000

    update photo data

--- client/src/data/photo-data.js
+++ client/src/data/photo-data.js
@@ -1,5 +1,36 @@
 export const Photo101 = [
   {
+    id: '31',
+    name: 'my DOG',
+    user: 'Jane Doe',
+    photo:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=0.672xw:1.00xh;0.166xw,0&resize=640:*',
+    caption: 'Took this photo'
+  },
+  {
+    id: '32',
+    name: 'my house',
+    user: 'Jane Doe',
+    photo: 'https://carlislehomes.com.au/static/images/hal/CARL607554_Matisse33_003_2.jpg',
+    caption:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aperiam, quibusdam accusantium perferendis obcaecati reprehenderit delectus in maiores necessitatibus facilis sequi enim optio quia et debitis fugit quis dolor ipsam unde nisi earum impedit reiciendis? Reiciendis libero rerum suscipit itaque obcaecati repudiandae, saepe eaque debitis nostrum voluptatem. Dolorem laborum tempore vel mollitia illum suscipit molestiae fugiat veritatis! Tempora illum error quia minima praesentium velit quidem hic. Deserunt illo at voluptates facere ipsum id blanditiis incidunt reprehenderit ad hic, sequi atque totam laudantium excepturi! Doloribus corrupti neque sapiente vitae sed ex, earum provident dignissimos cum minima tenetur vel maiores eveniet odio!'
+  },
+  {
+    id: '33',
+    name: 'my cat',
+    user: 'Jane Doe',
+    photo: 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+    caption: ''
+  },
+  {
+    id: '44',
+    name: 'my toy',
+    user: 'Jane Doe',
+    photo:
+      'https://cdn.shopify.com/s/files/1/0358/0310/3363/files/mobilo-toys-childplay-melbourne_x800.png?v=1654482053',
+    caption: ''
+  },
+  {
     id: '11',
     name: 'Porsche',
     user: 'John Doe',
@@ -45,6 +76,57 @@ export const Photo101 = [
   },
   {
     id: '16',
+    name: 'New kicks',
+    user: 'John Doe',
+    photo: 'https://i.pinimg.com/236x/72/27/4a/72274a00360daf41f358e11300f0c4ca.jpg',
+    caption: ''
+  },
+  {
+    id: '18',
+    name: 'Porsche',
+    user: 'John Doe',
+    photo: 'https://i.pinimg.com/236x/cb/89/9e/cb899e0693d298bc3f6121c069cfc1a1.jpg',
+    caption:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aperiam, quibusdam accusantium perferendis obcaecati reprehenderit delectus in maiores necessitatibus facilis sequi enim optio quia et debitis fugit quis dolor ipsam unde nisi earum impedit reiciendis? Reiciendis libero rerum suscipit itaque obcaecati!'
+  },
+  {
+    id: '19',
+    name: 'RasPi',
+    user: 'Jane Doe',
+    photo: 'https://i.pinimg.com/236x/37/fc/c8/37fcc851d7112e04b873ad8f826fb6a3.jpg',
+    caption:
+      'Reiciendis libero rerum suscipit itaque obcaecati repudiandae, saepe eaque debitis nostrum voluptatem. Dolorem laborum tempore vel mollitia illum suscipit molestiae fugiat veritatis! Tempora illum error quia minima praesentium velit quidem hic. Deserunt illo at voluptates facere ipsum id blanditiis incidunt reprehenderit ad hic, sequi atque totam laudantium excepturi! Doloribus corrupti neque sapiente vitae sed ex, earum provident dignissimos cum minima tenetur vel maiores eveniet odio!'
+  },
+  {
+    id: '20',
+    name: 'sony',
+    user: 'John Doe',
+    photo: 'https://i.pinimg.com/236x/e4/6f/0f/e46f0fb6ba7bf26740f41af3bd716550.jpg',
+    caption: 'found this in my local thrift store today what a classic'
+  },
+  {
+    id: '21',
+    name: 'Haikyuu',
+    user: 'John Doe',
+    photo: 'https://i.pinimg.com/236x/97/2f/b6/972fb63a745b4612b4214983e9abb196.jpg',
+    caption: 'someone hmu to play volley'
+  },
+  {
+    id: '22',
+    name: 'Good day',
+    user: 'John Doe',
+    photo: 'https://i.pinimg.com/236x/93/a7/fe/93a7feba74f4310e7a20cce84fd89719.jpg',
+    caption: 'saw some nice rainbow today'
+  },
+  {
+    id: '23',
+    name: 'baNaNa',
+    user: 'John Doe',
+    photo: 'https://i.pinimg.com/236x/fc/ff/e6/fcffe653d658c17af0f53248f883504e.jpg',
+    caption: ''
+  },
+  {
+    id: '24',
     name: 'New kicks',
     user: 'John Doe',
     photo: 'https://i.pinimg.com/236x/72/27/4a/72274a00360daf41f358e11300f0c4ca.jpg',
