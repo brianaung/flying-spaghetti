a4135cea76f0c6db72d74a87cc16bc8b580aaebe
NguyenPham26 <phamthanhnguyen1080@gmail.com>
2022-09-25 13:22:03 +1000

    photo page

--- client/src/pages/PhotoPage.js
+++ client/src/pages/PhotoPage.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import { Grid } from '@mui/material';
 
-import { Box, Stack } from '@mui/material';
-import PhotoCmt from '../components/PhotoCmt';
-import Comments from '../components/Comments';
+import { Stack, Box } from '@mui/system';
 
-function PhotoPage() {
+//icons
+import React, { useEffect, useRef, useState } from 'react';
+
+const PhotoSection = styled(Grid)({
+  background: 'red',
+  display: 'flex',
+  flexDirection: 'column',
+  margin: '40px',
+  width: '40%',
+  height: '30rem'
+});
+
+const CommentSection = styled(Stack)({
+  background: 'red',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  margin: '30px',
+  width: '60%',
+  gap: '20px'
+});
+
+const StyledBox = styled(Box)({
+  border: '1px solid',
+  width: '100%',
+  overflow: 'hidden'
+});
+
+export default function OnePhoto() {
+  const container = useRef(null);
+  const container1 = useRef(null);
+  const [imgHeight, setImgHeight] = useState(null);
+
+  useEffect(() => {
+    setImgHeight(container.current.height);
+  }, []);
+
   return (
-    <Box>
-      <Stack direction="row" spacing={2}>
-        <PhotoCmt></PhotoCmt>
-        <Comments></Comments>
-      </Stack>
-    </Box>
+    <>
+      <PhotoSection>
+        <Box bgcolor="black" height={`calc((100% - ${imgHeight}px)/2)`} ref={container1}>
+          fef
+        </Box>
+        <img
+          ref={container}
+          width="100%"
+          height="auto"
+          src="https://www.lifehacker.com.au/wp-content/uploads/sites/4/2021/05/13/GettyImages-822523842.jpg?quality=80&resize=1280,720"
+          alt="shiba"
+        />
+        <Box bgcolor="black" height={`calc((100% - ${imgHeight}px)/2)`}></Box>
+      </PhotoSection>
+      <CommentSection>
+        <StyledBox height="40%"></StyledBox>
+        <StyledBox height="60%"></StyledBox>
+      </CommentSection>
+    </>
   );
 }
-
-export default PhotoPage;
