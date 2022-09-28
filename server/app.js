35c0578cd7b8218068f5206520dee4f66fdb7322
Brian Aung <soekhantaung16@gmail.com>
2022-09-28 15:10:58 +1000

    remove frontend backend link

--- server/app.js
+++ server/app.js
@@ -1,15 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 // import userRouter from './routes/userRouter.js';
-// import router from './routes/router.js';
+import router from './routes/router.js';
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // const [isAuth, setIsAuth] = userState(false);
 app.use(cors());
@@ -22,19 +17,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// get the frontend build for deployment
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-app.get('/api', (req, res) => {
-  res.json({ message: '👋 from Express!' });
-});
-
-// catch all requests and return the static index.html file from react frontend
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-});
-
-/*
 app.use('/', router);
 
 // app.get('/', (req, res) => {
@@ -60,7 +42,6 @@ app.get('/upload', (req, res) => {
 app.get('/photo', (req, res) => {
   res.send('this is photo page');
 });
-*/
 
 /** ************************************* */
 
