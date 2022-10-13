99d5ad5ebd56587f3d20680cf761018229408819
Brian Aung <soekhantaung16@gmail.com>
2022-10-13 17:20:50 +1100

    add currently directory indicator

---
+++ client/src/components/Directory.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  // styled,
+  Typography,
+  Breadcrumbs,
+  Link
+} from '@mui/material';
+
+
+export default function Directory(props) {
+  return (
+    <>
+        {(props.currFolder == 'folders') ? (
+          <></>
+        ) : (
+          <Breadcrumbs fontSize="large">
+            <Link underline="hover" color="inherit" href="/dashboard/folders">Home</Link>
+            <Typography variant="h3" color="primary">{props.currFolder}</Typography>
+          </Breadcrumbs> 
+        )}
+    </>
+  )
+}
+
+Directory.propTypes = {
+  currFolder: PropTypes.string
+};
