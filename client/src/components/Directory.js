e311145d576fc866e82351ce645a827e95c63e71
Brian Aung <soekhantaung16@gmail.com>
2022-10-14 03:07:36 +1100

    use navigate instead of href for breadcrumbs

--- client/src/components/Directory.js
+++ client/src/components/Directory.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { React } from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {
   // styled,
@@ -8,13 +9,19 @@ import {
 } from '@mui/material';
 
 export default function Directory(props) {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate('/dashboard/folders');
+  }
+
   return (
     <>
       {props.currFolder == 'folders' ? (
         <></>
       ) : (
         <Breadcrumbs fontSize="large">
-          <Link underline="hover" color="inherit" href="/dashboard/folders">
+          <Link component="button" underline="hover" color="inherit" onClick={handleNavigation}>
             Home
           </Link>
           <Typography variant="h3" color="primary">
