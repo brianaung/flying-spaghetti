a30d2e12a2bcdb6d8d5b026e8d766c0ea07678be
Brian Aung <soekhantaung16@gmail.com>
2022-11-06 02:22:25 +1100

    fix some table formatting

--- client/src/components/UsersList.js
+++ client/src/components/UsersList.js
@@ -5,61 +5,68 @@ import { styled, Box, Button } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 
 const Container = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.background.main,
+  color: theme.palette.text.primary,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '10px',
   width: '100%',
   height: '100%',
   padding: '30px',
-  border: 'solid 1px',
-  borderColor: theme.palette.divider,
 }));
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 ,
+  { field: 'id', 
+    headerName: 'ID', 
+    width: 110,
     editable: true,
   },
   {
     field: 'firstName',
-    headerName: 'First name',
-    width: 150,
+    headerName: 'First Name',
+    width: 200,
     editable: false,
   },
   {
     field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
+    headerName: 'Last Name',
+    width: 200,
     editable: false,
   },
   {
-    field: 'age',
-    headerName: 'Age',
+    field: 'role',
+    headerName: 'Role',
+    width: 200,
+    editable: false,
+  },
+  {
+    field: 'capacity',
+    headerName: 'Capacity',
     type: 'number',
-    width: 110,
+    width: 170,
     editable: false,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
 
 export default function UsersList() {
   const { data } = useSelector((state) => state.users);
+
+  // TODO: use the fetched data to construct rows
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', capacity: 35, role: null },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 10, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
 
   const [selectionModel, setSelectionModel] = useState([]);
   const handleClick = () => {
@@ -70,12 +77,15 @@ export default function UsersList() {
   return (
     <Container>
       <DataGrid
+        sx={{
+          height: '80%',
+          width: '80%',
+        }}
         rows={rows}
         columns={columns}
         checkboxSelection
         onSelectionModelChange={(newSelection) => {
           setSelectionModel(newSelection)
-          console.log(newSelection);
         }}
         selectionModel={selectionModel}
         pageSize={9}
