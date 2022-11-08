3e03f24083de359eeced199b0ac1346c0e14f1c1
Brian Aung <soekhantaung16@gmail.com>
2022-11-08 18:19:57 +1100

    dont change header after sending res

--- server/controllers/contentController.js
+++ server/controllers/contentController.js
@@ -570,7 +570,6 @@ const createFolder = async (req, res, next) => {
     };
     await setDoc(doc(db, 'folders', req.body.folderName), folder);
     res.send(req.body.folderName);
-    res.sendStatus(200);
   } catch (error) {
     next(error);
   }
